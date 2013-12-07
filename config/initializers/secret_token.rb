8edeb14227926d3c012755a05aa48e189fbd211a
Melody Mustaine <mel.mustaine@gmail.com>
2013-12-07 08:33:30 -0800

    local setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carillon::Application.config.secret_key_base = '3d0f58f504bf56fcfe13fbbf3d90c783fef63802f39920f042d454ffc8d2013de0f044d07ad034711e2e77830f90fa062a1e7e07a78e82b9556cfdf51e68d3f1'
